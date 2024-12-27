682484f357c9f2674320ae25e15b6fd36c087620
Anastasiia <anastasia.milenkova.18@gmail.com>
2024-12-27 20:57:10 +0200

    Topic 3.5 Problem C started, not finished

---
+++ 3. Техники проектирования алгоритмов/3.5. Алгоритмы «Разделяй и властвуй»/Сортировка слиянием.py
@@ -0,0 +1,14 @@
+"""Реализуйте сортировку слиянием."""
+
+def merge_sort(li):
+    """Performs recursive merge sort"""
+    res = []
+    if len(li)==1:
+        res = list(li)
+    else:   # if the list li has more than 1 element then
+        pass
+
+    return res
+
+
+
