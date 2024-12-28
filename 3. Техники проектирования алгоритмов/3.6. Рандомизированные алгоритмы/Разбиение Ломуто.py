f72eff37f56330ee3710d3dd3935f334be7c1043
Anastasiia <anastasia.milenkova.18@gmail.com>
2024-12-28 21:17:26 +0200

    Topic 3.6 Lomuto's partition problem solved

--- 3. Техники проектирования алгоритмов/3.6. Рандомизированные алгоритмы/Разбиение Ломуто.py
+++ 3. Техники проектирования алгоритмов/3.6. Рандомизированные алгоритмы/Разбиение Ломуто.py
@@ -1,5 +1,25 @@
 """Постройте разбиение Ломуто относительно первого числа."""
 
-def lomutos_partition(li, pointer = 0):
+def lomutos_partition(li, pivot = 0):
     """Performs Lomuto's partition around pointer (0 by default)"""
-    
+    i = pivot
+    n = len(li)
+    for j in range(n):
+        if li[j]<=li[pivot]:
+            li[j], li[i] = li[i], li[j] 
+            i+=1
+    if n!=0:
+        li[pivot], li[i-1] = li[i-1], li[pivot]    
+
+# main program
+li = []
+n = int(input())
+inp = input().split()
+for el in inp:
+    li.append(int(el))
+
+lomutos_partition(li)
+for el in li:
+    print(el, end=" ")
+
+
