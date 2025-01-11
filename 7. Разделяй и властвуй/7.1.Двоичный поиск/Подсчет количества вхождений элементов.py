a03a396dd9cf75c3b4da936eac2f637a87e0144d
Anastasiia <anastasia.milenkova.18@gmail.com>
2025-01-11 22:38:30 +0100

    Topic 7.1 work in progress

--- 7. Разделяй и властвуй/7.1.Двоичный поиск/Подсчет количества вхождений элементов.py
+++ 7. Разделяй и властвуй/7.1.Двоичный поиск/Подсчет количества вхождений элементов.py
@@ -2,6 +2,24 @@
 Ваша задача --- для m значений qi​ необходимо найти количество вхождений qi​ в K
 """
 
+def lower_bound(len_li, li, key):
+    """
+    Returns the smallest index i such that li[i] >= key.
+    If all elements < key, returns len_li.
+    """
+    min_i = 0
+    max_i = len(li) - 1
+    while max_i>=min_i:
+        mid_i = (min_i + max_i)//2
+        if li[mid_i]==key:
+            inf = mid_i
+            max_i = mid_i - 1
+        elif li[mid_i]>key:
+            max_i = mid_i - 1
+
+
+
+
 def num_of_occurences(length_li, li, key):
     """Returns the number of occurences of the key in the list li"""
     inf = -1
@@ -18,11 +36,16 @@ def num_of_occurences(length_li, li, key):
             max_ind = mid_ind - 1
         else:   # if li[mid_ind]<key
             min_ind = mid_ind + 1
+    # from here on out we have scanned the entire array (max_ind>min_ind)
+    # and found the inferior border
+
+    # resetting the borders
+    min_ind = inf
     if sup!=-1:
         max_ind = sup
-    else:
+    else:   # if we haven't found the superior border
         max_ind = length_li - 1
-    min_ind = inf
+    
     while max_ind>=min_ind:
         mid_ind = (min_ind + max_ind)//2
         if li[mid_ind]==key:
@@ -32,14 +55,14 @@ def num_of_occurences(length_li, li, key):
             max_ind = mid_ind - 1
         else:   # if li[mid_ind]<key
             min_ind = mid_ind + 1
-    if sup==-1 or inf == -1:
-        return 0
-    return sup - inf + 1
     # from here on out we have scanned the entire array (max_ind>min_ind)
+    # and found the superior border
+    if sup==-1 or inf==-1:  # if we haven't one of the borders then
+        res = 0
+    else:   # if we have calculated the borders  then
+        res = (sup - inf) + 1
+    return res
     
-
-
-
 # main program
 n_elements = int(input())
 li = [int(x) for x in input().split()]
