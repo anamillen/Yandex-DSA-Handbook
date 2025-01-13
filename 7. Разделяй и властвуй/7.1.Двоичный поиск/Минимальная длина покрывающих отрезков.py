3ccb4e781bb7f49c75299f2c8dad81b7a9afa5e3
Anastasiia <anastasia.milenkova.18@gmail.com>
2025-01-13 06:54:37 +0100

    Topic 7.1 Problem D divide and conquer solution added

--- 7. Разделяй и властвуй/7.1.Двоичный поиск/Минимальная длина покрывающих отрезков.py
+++ 7. Разделяй и властвуй/7.1.Двоичный поиск/Минимальная длина покрывающих отрезков.py
@@ -4,28 +4,42 @@
 
 Необходимо определить, какую минимальную длину могут иметь отрезки.
 """
+def can_cover_with_k(segment_len, K, points, N_POINTS):
+    """Returns a boolean indicating if all points can be covered with K segments
+    of length segment_len"""
+    can_cover = False
+    n_segments = 0
+    i_point = 0
+    while n_segments < K and not can_cover:
+        covered = points[i_point]
+        while i_point < N_POINTS and points[i_point] <= covered + segment_len:
+            i_point += 1
+        # from here on out it's either i_point==N_POINTS (we have covered all the points)
+        # or points[i_point] > covered + segment_len     (we need more segments)
+        if i_point == N_POINTS:
+            can_cover = True
+        n_segments += 1
 
-def min_len_of_segments(N_POINTS, MAX_SEGMENTS, points):
+    # from here on out it's n_segments==K                (we have used up all available segments)
+    # or can_cover
+    return can_cover
+
+def min_len_of_segments(MAX_SEGMENTS, points, N_POINTS):
     """Returns the minimum length of k segments to cover all the points"""
     points = sorted(points)
-    all_covered = False
-    segment_len = 0
-    while not all_covered:
-        i_point = 0
-        segment_count = 0
-        while segment_count<MAX_SEGMENTS and i_point<N_POINTS:
-            covered_to = points[i_point]+segment_len
-            while i_point<N_POINTS and points[i_point]<=covered_to:
-                i_point+=1
-            # from here on out we have moved further by 1 segment
-            segment_count+=1
-        # from here on out we have laid all the k segments
-        if i_point==N_POINTS and segment_count<=MAX_SEGMENTS:   # if we have covered all the points then
-            all_covered = True
-        else:   # if not, we increment the segment length
-            segment_len+=1
-    # from here on out we have covered all the points (all_covered==True) with k (or less) segments
-    return segment_len
+    # binary search bounds: lower and higher length bounds
+    low = 0
+    high = points[-1] - points[0]
+
+    while low < high:
+        mid = (high + low) // 2
+        if can_cover_with_k(mid, MAX_SEGMENTS, points, N_POINTS):
+            high = mid
+        else:   # if the length is too short
+            low = mid + 1
+    # here low >= high
+
+    return low  # returning the lowest distance calculated
 
 # main program
 n_points, n_segments = [int(x) for x in input().split()]
