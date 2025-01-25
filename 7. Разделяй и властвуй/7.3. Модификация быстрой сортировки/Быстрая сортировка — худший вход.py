73a5cfd64c2a6ad5e3c10f43a715b0c16be1143c
Anastasiia <anastasia.milenkova.18@gmail.com>
2025-01-25 19:43:43 +0100

    Topic 7.3 problem b attempt

--- 7. Разделяй и властвуй/7.3. Модификация быстрой сортировки/Быстрая сортировка — худший вход.py
+++ 7. Разделяй и властвуй/7.3. Модификация быстрой сортировки/Быстрая сортировка — худший вход.py
@@ -6,20 +6,23 @@
 считайте, что точка m выбирается по формуле m = ⌊( l + r ) / 2 ⌋
 """
 
-def deepest_recursion_data(seq, e):
+def deepest_recursion_data(seq, start, end, e):
     """Returns a sequence of integers for which quick sort algorithm 
     has the worst time complexity"""
-    len_seq = len(seq)
-    m = len_seq // 2
+    len_seq = (end - start) + 1
+    m = (start + end) // 2
     if len_seq == 1:
         seq[m] = e
-    elif len_seq != 0:   # if the sequence has more than 1 element then
+    elif len_seq == 0:   
+        e += 1
+    else: # if the sequence has more than 1 element then
         seq[m] = e
-        e = deepest_recursion_data(seq[:m], e - 1)
-        deepest_recursion_data(seq[m+1:], e - 1)
+        e = deepest_recursion_data(seq, start, m - 1, e - 1)
+        e = deepest_recursion_data(seq, m+1, end, e - 1)
     return e
 
 n = int(input())
 res = [0]*n
-print(deepest_recursion_data(res, n))
-print(res)
+deepest_recursion_data(res, 0, n-1, n)
+for el in res:
+    print(el, end=" ")
