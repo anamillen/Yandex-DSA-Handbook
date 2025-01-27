0bb2d6c663c2fd2d01f81e333fc5c5bd69dec71c
Anastasiia <anastasia.milenkova.18@gmail.com>
2025-01-27 06:35:28 +0100

    Topic 7.3 problem B solution fails on test 5

--- 7. Разделяй и властвуй/7.3. Модификация быстрой сортировки/Быстрая сортировка — худший вход.py
+++ 7. Разделяй и властвуй/7.3. Модификация быстрой сортировки/Быстрая сортировка — худший вход.py
@@ -6,9 +6,10 @@
 считайте, что точка m выбирается по формуле m = ⌊( l + r ) / 2 ⌋
 """
 
+"""
 def deepest_recursion_data(seq, start, end, e):
-    """Returns a sequence of integers for which quick sort algorithm 
-    has the worst time complexity"""
+    \"""Returns a sequence of integers for which quick sort algorithm 
+    has the worst time complexity\"""
     len_seq = (end - start) + 1
     m = (start + end) // 2
     if len_seq == 1:
@@ -21,8 +22,43 @@ def deepest_recursion_data(seq, start, end, e):
         e = deepest_recursion_data(seq, m+1, end, e - 1)
     return e
 
-n = int(input())
-res = [0]*n
-deepest_recursion_data(res, 0, n-1, n)
-for el in res:
+"""
+
+def worst_case(seq, length):
+    """Finds and returns the worst case scenario for the quick sort algorithm (time-wise)"""
+    l = 0; r = length - 1
+    small_e = 1; large_e = length
+    place_largest = True
+
+    def deepest_recursion_data(seq, l, r):
+        """Returns a sequence of integers for which the quick sort algorithm 
+        has the worst time complexity"""
+        nonlocal small_e, large_e, place_largest
+        if l <= r:
+
+            m = (l + r) // 2
+            
+            if place_largest:
+                seq[m] = large_e
+                large_e -= 1
+            else:   # if place_largest is false then
+                seq[m] = small_e
+                small_e += 1
+            
+            place_largest = not place_largest
+                
+            deepest_recursion_data(seq, m + 1, r)
+            deepest_recursion_data(seq, l, m - 1)
+        
+    deepest_recursion_data(seq, l, r)
+
+
+
+
+
+
+length = int(input())
+seq = [0]*length
+worst_case(seq, length)
+for el in seq:
     print(el, end=" ")
