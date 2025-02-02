2ad81a18600951f307a9b425ae416af454c88278
Anastasiia <anastasia.milenkova.18@gmail.com>
2025-02-02 14:58:04 +0100

    Topic 7.4 seemingly found a solution for problem D, but fails the TL on test 13

--- 7. Разделяй и властвуй/7.4. Подсчёт инверсий/Подсчет транспозиций (усложненная).py
+++ 7. Разделяй и властвуй/7.4. Подсчёт инверсий/Подсчет транспозиций (усложненная).py
@@ -1,3 +1,75 @@
 """
+Дана перестановка p из n целых чисел. Необходимо вычислить, какое минимальное количество транспозиций требуется совершить, 
+чтобы упорядочить элементы перестановки хорошо.
 
+Будем говорить, что элементы перестановки упорядочены хорошо, если найдется такое число x от 1 до n, 
+что перестановка имеет вид x, x+1, …, n, 1, 2, …, x−1.
+
+Транспозиция -- это обмен соседних элементов перестановки p местами.
 """
+def merge(left, right, splt, length):
+    """Merges 2 lists left and right and returns the number of inversions effectuated"""
+    
+    srtd = []
+    inversions = 0
+    l = 0; r = 0
+
+    while l < splt and r < length - splt:
+        
+        if left[l] <= right[r]:
+
+            srtd.append(left[l])
+            l += 1
+
+        else:   # if right[r] < left[l]
+
+            srtd.append(right[r])
+            r += 1
+            inversions = inversions + splt - l
+
+    # here either l == splt or r ==  length (one of the array has ended)
+    
+    srtd += left[l:]
+    srtd += right[r:]
+
+    return srtd, inversions
+
+
+
+def count_T(length, lst):
+
+    min_T = length * (length - 1) / 2 + 1
+
+    for x in range(1, length + 1):
+
+        new = [ a - x if a >= x else a - x + length for a in lst]
+        num_T = count_inversions(length, new)[1]
+
+        if num_T < min_T:
+
+            min_T = num_T
+
+    return min_T
+
+
+def count_inversions(length, lst):
+    """Returns the number of inversions in lst"""
+
+    inversions = 0
+    srtd = lst
+
+    if length > 1:
+        
+        splt = length // 2
+        left, left_inv = count_inversions(splt, lst[:splt])
+        right, right_inv = count_inversions(length - splt, lst[splt:])
+        srtd, splt_inv = merge(left, right, splt, length)
+        inversions = left_inv + right_inv + splt_inv 
+
+    return srtd, inversions
+
+# main program
+length = int(input())
+lst = [int(x) for x in input().split()]
+inversions = count_T(length, lst)
+print(inversions)
